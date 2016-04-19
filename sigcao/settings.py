2bfbe98b191393211b154e1a57e2986ac6fda332
Vinicius Moraes <vsantana0212@gmail.com>
2016-04-19 11:00:41 -0400

    Tentativa 4.

--- sigcao/settings.py
+++ sigcao/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import urlparse
+import urllib.parse as urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
