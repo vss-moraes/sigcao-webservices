410592d0de894f8e0752426b4f1f254bdeeee537
Vinicius Moraes <vsantana0212@gmail.com>
2016-04-19 11:20:40 -0400

    tentativa 5

--- sigcao/wsgi.py
+++ sigcao/wsgi.py
@@ -10,34 +10,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sigcao.settings")
-
-application = get_wsgi_application()
 """
 
-sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = '<app-name>.settings'
-
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
-
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
-
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'sigcao'))
+os.environ["DJANGO_SETTINGS_MODULE"] = "sigcao.settings"
 
 application = get_wsgi_application()
