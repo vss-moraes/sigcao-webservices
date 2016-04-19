85c2890ce66a08e5643e7605e840f01ab0b83450
Vinicius Moraes <vsantana0212@gmail.com>
2016-04-19 14:29:19 -0400

    Configuração BD

--- sigcao/settings.py
+++ sigcao/settings.py
@@ -86,14 +86,21 @@ WSGI_APPLICATION = 'sigcao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sigcao',
+#         'USER': 'vsant',
+#         'PASSWORD': 'batman',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sigcao',
-        'USER': 'vsant',
-        'PASSWORD': 'batman',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
