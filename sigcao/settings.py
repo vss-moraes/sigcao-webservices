20ff868016571bffad2e12cc1610ddf595365fbf
Vinicius Moraes <vsantana0212@gmail.com>
2016-05-16 08:19:05 -0400

    Settings configurado para deploy no heroku.

--- sigcao/settings.py
+++ sigcao/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^m2ib3d72y@xl@8a!+hb+ycyrjvyq3*pz8fa_bi*!3$4)lezh7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vss-moraes.github.io']
 
 
 # Application definition
