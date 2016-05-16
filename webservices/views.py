671cd17cb67077fa8e377c5066a5c1cc37666ea2
Vinicius Moraes <vsantana0212@gmail.com>
2016-05-16 07:59:43 -0400

    Filtros implementados corretamente.

--- webservices/views.py
+++ webservices/views.py
@@ -1,32 +1,35 @@
 from webservices.models import Ocorrencia
 from webservices.serializers import OcorrenciaSerializer
 from rest_framework import generics
-from itertools import chain
+import json
 
 
 class FiltraOcorrencias(generics.ListAPIView):
     serializer_class = OcorrenciaSerializer
 
-    def get_queryset(self):
-        queryset = Ocorrencia.objects.all()
+    def termos_da_busca(self, nome_do_campo, nome_arquivo):
+        parametros_request = self.request.query_params.getlist(nome_do_campo)
+        if len(parametros_request) > 0:
+            return parametros_request
 
+        with open (nome_arquivo, 'r') as arquivo:
+            lista = json.load(arquivo)
+
+        return [item['fields']['nome'] for item in lista]
+
+    def get_queryset(self):
+        racas = self.termos_da_busca('raca', 'fixtures/lista_racas.json')
+        faixas_etarias = self.termos_da_busca('faixa_etaria', 'fixtures/lista_faixa_etaria.json')
+        doencas = self.termos_da_busca('doenca', 'fixtures/lista_doencas.json')
         sexo = self.request.query_params.get('sexo', None)
-        faixas_etarias = self.request.query_params.getlist('faixa_etaria')
-        racas = self.request.query_params.getlist('raca')
-        doencas = self.request.query_params.getlist('doenca')
+
+        queryset = Ocorrencia.objects.filter(raca__nome__in=racas).filter(doenca__nome__in=doencas).filter(faixa_etaria__nome__in=faixas_etarias)
 
         if sexo is not None:
-            queryset = list(queryset.filter(sexo__nome=sexo))
-        if len(doencas) > 0:
-            queryset = [item for item in queryset if str(item.doenca) in doencas]
-        if len(faixas_etarias) > 0:
-            queryset = [item for item in queryset if str(item.faixa_etaria) in faixas_etarias]
-        if len(racas) > 0:
-            queryset = [item for item in queryset if str(item.raca) in racas]
+            queryset = queryset.filter(sexo__nome=sexo)
 
         return queryset
 
 
 class EnviaOcorrencia(generics.CreateAPIView):
-    queryset = Ocorrencia.objects.all()
     serializer_class = OcorrenciaSerializer
