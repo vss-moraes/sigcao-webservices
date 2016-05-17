15a2ea7df0667fcae67bb16a983b37afacb78a21
Vinicius Moraes <vsantana0212@gmail.com>
2016-05-17 11:28:54 -0400

    settings.py configurado

--- sigcao/settings.py
+++ sigcao/settings.py
@@ -80,6 +80,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sigcao.wsgi.application'
 
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+#Configuração para banco de dados SQLite3
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
@@ -98,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
@@ -110,6 +123,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
