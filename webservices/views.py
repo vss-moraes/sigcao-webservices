5a5b77f8fdfe5f867f3243d9e7da6568e4eedb73
Vinicius Moraes <vsantana0212@gmail.com>
2016-06-14 16:41:58 -0400

    Resultado da busca vazia alterado

--- webservices/views.py
+++ webservices/views.py
@@ -12,7 +12,7 @@ class FiltraOcorrencias(generics.ListAPIView):
         if len(parametros_request) > 0:
             return parametros_request
 
-        with open (nome_arquivo, 'r') as arquivo:
+        with open(nome_arquivo, 'r') as arquivo:
             lista = json.load(arquivo)
 
         return [item['fields']['nome'] for item in lista]
@@ -22,15 +22,16 @@ class FiltraOcorrencias(generics.ListAPIView):
         faixas_etarias = self.termos_da_busca('faixa_etaria', 'fixtures/lista_faixa_etaria.json')
         doencas = self.termos_da_busca('doenca', 'fixtures/lista_doencas.json')
         sexo = self.request.query_params.get('sexo', None)
-        lat = self.request.query_params.getlist('lat')
-        lng = self.request.query_params.getlist('lng')
 
-        queryset = Ocorrencia.objects.filter(raca__nome__in=racas).filter(doenca__nome__in=doencas).filter(faixa_etaria__nome__in=faixas_etarias)
+        print (self.request.query_params)
+
+        if self.request.query_params.get('doenca', None):
+            queryset = Ocorrencia.objects.filter(raca__nome__in=racas).filter(doenca__nome__in=doencas).filter(faixa_etaria__nome__in=faixas_etarias)
+        else:
+            queryset = None
 
         if sexo is not None:
             queryset = queryset.filter(sexo__nome=sexo)
-        if len(lat) > 0:
-            queryset = queryset.filter(longitude__range=(lng[1], lng[0])).filter(latitude__range=(lat[1], lat[0]))
 
         return queryset
 
